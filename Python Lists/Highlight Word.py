881ca74edaae2253e4e3d263cb5b6840614b15c9
Deepesh Garg <deepeshrishu09@gmail.com>
2020-07-27 23:47:59 +0530

    Added the Highlight Word.py file.

---
+++ Python Lists/Highlight Word.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def highlight_word(sentence, word):
+	return(sentence.replace(word,word.upper()))
+
+print(highlight_word("Have a nice day", "nice"))
+print(highlight_word("Shhh, don't be so loud!", "loud"))
+print(highlight_word("Automating with Python is fun", "fun"))
+
