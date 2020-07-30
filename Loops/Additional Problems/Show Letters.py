0ef5882a26db1a7659aa2c8bbf495b847233ad3c
Deepesh Garg <deepeshrishu09@gmail.com>
2020-07-30 23:43:32 +0530

    Added the Show Letters.py file.

---
+++ Loops/Additional Problems/Show Letters.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def show_letters(word):
+  
+	for letter in range(len(word)) :
+		print(word[letter])
+		
+
+show_letters("Hello")
+# Should print one line per letter
+
