7ce5c1bb766fd713fe9d028f801c7a7e94499a47
Deepesh Garg <deepeshrishu09@gmail.com>
2020-07-31 23:16:44 +0530

    Added the Group Per User.py file.

---
+++ Python Dictionaries/Groups Per User.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def groups_per_user(group_dictionary):
+	user_groups = {}
+	# Go through group_dictionary
+	for u,g in group_dictionary.items():
+		# Now go through the users in the group
+		for user in g:
+		  if user not in user_groups:
+		    user_groups[user]=[]
+		  user_groups[user].append(u)
+			# Now add the group to the the list of
+# groups for this user, creating the entry
+# in the dictionary if necessary
+      
+	return(user_groups)
+
+print(groups_per_user({"local": ["admin", "userA"],
+		"public":  ["admin", "userB"],
+		"administrator": ["admin"] }))
+
+
+# In[ ]:
+
+
+
+
